39ad2d602bbddf8135070d5cb7f528a5899180b1
q292u <q292u@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-03 08:11:50 +0000

    Updated Readme file and moved to v1.2
    git-svn-id: https://plugins.svn.wordpress.org/text-file-widget/tags/1.2@1211636 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Text File Widget ===
+Contributors: q292u
+Tags: Text File, Widget
+Donate link: http://www.http://q292u.com/plugins/text-file-widget/
+Requires at least: 3.7
+Tested up to: 4.2.2
+Stable tag: 1.2
+License: GPL v2 or Later
+
+Displays the contents of a text file as a widget
+
+== Description ==
+
+This simple plugin allows you to display the contents of a text file as a widget, in any widgetized area provided by your theme (eg Sidebar)
+
+Only text is displayed - HTML is stripped out.
+
+End-of-line characters are displayed as HTML "Breaks" 
+
+== Installation ==
+There are three possibilities:
+Use the standard Wordpress Plugins > Add New procedure (Recommended)
+or
+Download the plugin file from Wordpress.org and the Upload the Zip file
+Add Plugin > Upload
+or
+Download the plugin file from Wordpress.org, then manually upload the unzipped folder (text-file-widget) into wp-content/plugins
+
+Then
+Activate the plugin 
+
+Now there will be a \"text file widget\" in the Appearance>Widgets section. Drag this to a widget location, and tell it which file you want to display and what title to give it (or none)
+
+You can have multiple instances displaying different files. 
+
+== Frequently Asked Questions ==
+= Why is HTML stripped out? =
+
+This plugin is only intended to display text. Hence the name.
+
+== Screenshots ==
+1. These are the widget options
+2. This is the widget in action
+
+== Changelog ==
+1.0 - Initial working version
---
+++ text-file-widget.php
@@ -0,0 +1,75 @@
+<?php
+/**
+* @package TextFileWidget
+* @version 1.2
+*/
+/* Plugin Name: Text File Widget 
+Plugin URI: http://www.q292u.com
+Description: Display contents of a text file as a sidebar widget
+Version: 1.2
+Author: Paul Rak
+Author URI: http://www.q292u.com
+*/
+
+class TextFileWidget extends WP_Widget {
+          function TextFileWidget() {
+                    $widget_ops = array(
+                    'classname' => 'TextFileWidget',
+                    'description' => 'Display contents of a text file as a sidebar widget'
+          );
+
+          $this->WP_Widget(
+                    'TextFileWidget',
+                    'Text File Widget',
+                    $widget_ops
+          );
+} //end function textfilewidget
+
+function widget($args, $instance) { // widget sidebar output
+                    extract($args);
+                    $title 		= apply_filters('widget_title', $instance['title']);
+        			$filename 	= $instance['filename'];
+                    echo $before_widget; // pre-widget code from theme
+                    if ( $title )
+                    {
+                        echo $before_title . $title . $after_title;
+                    }
+                    $txt=file_get_contents($filename);
+                    $txt=nl2br($txt); // translate end-of-line to html..
+	                $txt=strip_tags($txt,'<br><br/>');	// strip all tags except line breaks..                    
+					echo "<ul><li>".$txt."</li></ul>";
+                    echo $after_widget; // post-widget code from theme
+}
+
+/** @see WP_Widget::update -- do not rename this */
+function update($new_instance, $old_instance) {		
+		$instance = $old_instance;
+		$instance['title']    = strip_tags($new_instance['title']);
+		$instance['filename'] = strip_tags($new_instance['filename']);
+        return $instance;
+}
+ 
+/** @see WP_Widget::form -- do not rename this */
+function form($instance) {	
+ 
+        $title 		= esc_attr($instance['title']);
+        $filename	= esc_attr($instance['filename']);
+        ?>
+         <p>
+          <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> 
+          <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+		  <label for="<?php echo $this->get_field_id('filename'); ?>"><?php _e('Filename:'); ?></label> 
+          <input class="widefat" id="<?php echo $this->get_field_id('filename'); ?>" name="<?php echo $this->get_field_name('filename'); ?>" type="text" value="<?php echo $filename; ?>" />
+        </p>
+        <?php 
+}
+}//end class
+
+add_action(
+          'widgets_init',
+          create_function('','return register_widget("TextFileWidget");')
+);
+
+
+ 
+?>
